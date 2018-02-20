9f773e63c4748f41f3ce7756e976e6bf236abcd1
vlad <w.wisnewsky@gmail.com>
2018-02-20 18:53:20 +0200

    Basic spell stup

---
+++ dnd/main.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+  $('.main-body').load('./spells.html');
+  $(document).on('click', '.header-tab__spells-db', function() {
+    $('.main-body').load('./spells.html');
+  });
+});
