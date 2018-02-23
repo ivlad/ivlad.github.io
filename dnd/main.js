b130d0d1ad76b5f3ab46c44cfe2463b41e694080
vlad <w.wisnewsky@gmail.com>
2018-02-23 09:18:35 +0200

    Small cleanup

--- dnd/main.js
+++ dnd/main.js
@@ -6,43 +6,40 @@ $( document ).ready(function() {
   });
 });
 
-const vriteChosenSpells = function() {
+let preparedSpells = [];
+
+const vriteChosenSpells = () => {
   $('.prepared-spells').html('');
-  preparedSpells.forEach(function(val) {
+  preparedSpells.forEach((val) => {
     $('.prepared-spells').append(`
       <div class="prepared-spell">
-        <span class="remove-spell" data-name="${val.name}">(X)</span> 
+        <span class="remove-spell" data-name="${val.name}">❌</span> 
         <strong>${val.name.replace(/-/g, ' ')}</strong> - ${val.level}
       </div>
     `);
   });
 };
 
-let preparedSpells = [];
-
 function ChosenSpell(name, level) {
   this.name = name;
   this.level = level;
 }
 
-const markChosenSpells = function() {
+const markChosenSpells = () => {
   $('.spell').removeClass('already-chosen');
-  preparedSpells.forEach(function(val) {
+  preparedSpells.forEach((val) => {
     $('#' + val.name).addClass('already-chosen');
   });
 };
 
-function SortByLevel(a, b){
-  var aLevel = a.level;
-  var bLevel = b.level; 
+const SortByLevel = (a, b) => {
+  const aLevel = a.level;
+  const bLevel = b.level; 
   return ((aLevel < bLevel) ? -1 : ((aLevel > bLevel) ? 1 : 0));
-}
+};
 
-
+// prepare spell
 $(document).on('click', '.prepare-spell', function(){
-  if ($(this).hasClass('.already-chosen')) {
-    return false;
-  }
   const spell = $(this).closest('.spell').attr('id');
   const level = $(this).closest('.spell-container').data('level');
   const chosenSpell = new ChosenSpell(spell, level);
@@ -53,16 +50,18 @@ $(document).on('click', '.prepare-spell', function(){
   return false;
 });
 
+// remove spell
 $(document).on('click', '.remove-spell', function() {
   const spellName = $(this).data('name');
-  function isSpell(spell) { 
-    return spell.name === spellName;
-  }
+  const isSpell = (spell) => {
+    return spell.name === spellName; 
+  };
   const spellToRemove = preparedSpells.find(isSpell);
-
-  preparedSpells = jQuery.grep(preparedSpells, function(value) {
-    return value != spellToRemove;
+  $(this).closest('.prepared-spell').fadeOut(() => {
+    preparedSpells = $.grep(preparedSpells, (spell) => {
+      return spell != spellToRemove;
+    });
+    vriteChosenSpells();
+    markChosenSpells();
   });
-  vriteChosenSpells();
-  markChosenSpells();
 });
